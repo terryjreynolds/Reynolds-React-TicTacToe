afffe933cd19d27993228f041f3ca38a62d6857d
Terry Reynolds <terrancejreynolds@gmail.com>
2018-08-21 22:03:39 -0400

    got emptyIndexies to work. Sorting out minimax level 2 play is next

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./Footer";
 import Scoreboard from "./Scoreboard";
-
+import { emptyIndexies } from "../helpers";
 import GameBoard from "./GameBoard";
 import PlayButtons from "./PlayButtons";
 
--- src/helpers.js
+++ src/helpers.js
@@ -15,3 +15,74 @@ export function objectMaker(el1, el2, el3) {
   };
   return stateObject;
 }
+export function emptyIndexies(board) {
+  let array = [];
+  board.map(c => c !== "O" && c !== "X").map((c, i) => c && array.push(i));
+  return array;
+}
+/* export function minimax(newBoard, player) {
+  //available spots
+  var availSpots = emptyIndexies(newBoard);
+
+  // checks for the terminal states such as win, lose, and tie and returning a value accordingly
+  if (winning(newBoard, Player)) {
+    return { score: -10 };
+  } else if (winning(newBoard, Computer)) {
+    return { score: 10 };
+  } else if (availSpots.length === 0) {
+    return { score: 0 };
+  }
+
+  // an array to collect all the objects
+  var moves = [];
+
+  // loop through available spots
+  for (var i = 0; i < availSpots.length; i++) {
+    //create an object for each and store the index of that spot that was stored as a number in the object's index key
+    var move = {};
+    move.index = newBoard[availSpots[i]];
+
+    // set the empty spot to the current player
+    newBoard[availSpots[i]] = player;
+
+    //if collect the score resulted from calling minimax on the opponent of the current player
+    if (player == Computer) {
+      var result = minimax(newBoard, Player);
+      move.score = result.score;
+    } else {
+      var result = minimax(newBoard, Computer);
+      move.score = result.score;
+    }
+
+    //reset the spot to empty
+    newBoard[availSpots[i]] = move.index;
+
+    // push the object to the array
+    moves.push(move);
+  }
+
+  // if it is the computer's turn loop over the moves and choose the move with the highest score
+  var bestMove;
+  if (player === Computer) {
+    var bestScore = -10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    // else loop over the moves and choose the move with the lowest score
+    var bestScore = 10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+
+  // return the chosen move (object) from the array to the higher depth
+  return moves[bestMove];
+}
+ */
