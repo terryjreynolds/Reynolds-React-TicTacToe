02d161c82bd9c41c602768bb52ac1b6195940cbc
Terry Reynolds <terrancejreynolds@gmail.com>
2020-11-21 11:21:52 -0500

    added new personal website link and jsx for current year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+ const year = new Date().getFullYear()
 class Footer extends React.Component {
+ 
   render() {
     return (
-      <footer className="footer">
-        Tic Tac Toe in React designed and coded by T. Reynolds, 2018.
+      <footer className="footer" >
+        Tic Tac Toe in React designed and coded by <a id="portfolioLink" target="blank" href="https://terryjreynolds.dev">T. Reynolds</a>, &copy; {year}.
       </footer>
     );
   }
